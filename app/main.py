c38230df6074135ab0bb73a853cc2536489b7b7f
bigidulka <71977926+bigidulka@users.noreply.github.com>
2024-10-31 02:50:54 +0300

    serv

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@
 import logging
 from fastapi import FastAPI
 from src.endpoints.routes import router
-from pyngrok import ngrok
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -11,12 +10,6 @@ app = FastAPI()
 
 app.include_router(router)
 
-port = 8000
-public_url = ngrok.connect(port)
-logger.info(f"ngrok запущен: {public_url}")
-
-@app.on_event("shutdown")
-def shutdown_event():
-    """Закрываем ngrok туннель при завершении приложения."""
-    ngrok.disconnect(public_url)
-    ngrok.kill()
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
