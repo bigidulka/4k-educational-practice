d23b36605da81b1f288f20fc5a90f43341e0dd64
bigidulka <71977926+bigidulka@users.noreply.github.com>
2024-11-11 01:15:08 +0300

    time fix

--- app/main.py
+++ app/main.py
@@ -3,10 +3,7 @@ import logging
 from fastapi import FastAPI
 from src.endpoints.routes import router
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s"  # добавили дату и время
-)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
@@ -15,4 +12,29 @@ app.include_router(router)
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+        log_level="info",
+        log_config={
+            "version": 1,
+            "formatters": {
+                "default": {
+                    "format": "%(asctime)s - %(levelname)s - %(message)s",
+                    "datefmt": "%Y-%m-%d %H:%M:%S",
+                },
+            },
+            "handlers": {
+                "default": {
+                    "formatter": "default",
+                    "class": "logging.StreamHandler",
+                },
+            },
+            "root": {
+                "level": "INFO",
+                "handlers": ["default"],
+            },
+        }
+    )
